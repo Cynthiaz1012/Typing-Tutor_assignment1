26cc5e5ff9ee050c7467808a3448a5c582d3c907
Cynthia <ouocwouo@gmail.com>
2026-09-23 23:33:49 -0400

    add all keyboard button

---
+++ src/main/java/com/mycompany/assignment_1/App.java
@@ -0,0 +1,81 @@
+package com.mycompany.assignment_1;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ * @author Cynthia Zhang
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Button[] keyButtons = {
+            new Button("1"),
+            new Button("2"),
+            new Button("3"),
+            new Button("4"),
+            new Button("5"),
+            new Button("6"),
+            new Button("7"),
+            new Button("8"),
+            new Button("9"),
+            new Button("0"),
+            new Button("-"),
+            new Button("="),
+            new Button("Backspace"),
+
+            new Button("Tab"),
+            new Button("Q"),
+            new Button("W"),
+            new Button("E"),
+            new Button("R"),
+            new Button("T"),
+            new Button("Y"),
+            new Button("U"),
+            new Button("I"),
+            new Button("O"),
+            new Button("P"),
+            new Button("["),
+            new Button("]"),
+
+            new Button("Caps"),
+            new Button("A"),
+            new Button("S"),
+            new Button("D"),
+            new Button("F"),
+            new Button("G"),
+            new Button("H"),
+            new Button("J"),
+            new Button("K"),
+            new Button("L"),
+            new Button("Enter"),
+
+            new Button("Shift"),
+            new Button("Z"),
+            new Button("X"),
+            new Button("C"),
+            new Button("V"),
+            new Button("B"),
+            new Button("N"),
+            new Button("M"),
+            new Button(","),
+            new Button("."),
+            new Button("Shift"),
+
+            new Button("Ctrl"),
+            new Button("Alt"),
+            new Button("Space"),
+            new Button("Alt"),
+            new Button("Ctrl")
+        };
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
